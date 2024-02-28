fc945a82aa6e487fd3a46c0493987fc11dc14764
eldripper <eliasgithinji922@gmail.com>
2024-02-28 19:33:54 +0300

    dictionary_car, dictionary, tuple_hobbies

---
+++ week_3/dictionary_car.py
@@ -0,0 +1,28 @@
+# about my favorite car
+# date : 28/02/2024
+# name : elias githinji
+
+
+car = {"make":"bugatti chiron","colour":"black","horsepower":"1,500","date of birth":"2006"}
+
+
+print(car["make"])
+print(car["horsepower"])
+print(car["date of birth"])
+
+car["horsepower"] = "2,500"
+car["date of birth"] = "2022"
+
+print(car)
+
+
+bro_car = car.copy()# copy dictionary
+
+
+
+for key, value in car.items():
+    print(key)
+    print("\t")
+    print(value)
+
+
---
+++ week_3/tuble_hobbies.py
@@ -0,0 +1,10 @@
+# type a list that does not change
+# date : 28/02/2024
+# name : elias
+
+hobbies = ("cooking","dancing","singing","swimming")
+print(hobbies)
+
+
+for hobby in hobbies :
+    print(hobbies)
